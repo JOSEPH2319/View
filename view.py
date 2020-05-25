71f4fa557aa2c750a070fc67917a114c0ce7c258
JOSEPH2319 <54958447+JOSEPH2319@users.noreply.github.com>
2020-05-24 23:52:01 -0400

    Update view.py

--- view.py
+++ view.py
@@ -4,6 +4,7 @@ conn = sqlite3.connect('chinook.sqlite')
 library = conn.cursor()
 
 
+# Question #1
 CREATE VIEW
 EXAMPLE AS
 SELECT *
@@ -11,20 +12,27 @@ FROM library
 WHERE name LIKE 'Grunge';
 
 
+# Question #2
 SELECT MAX(money)
 FROM library
 GROUP BY TV_SHOW;
 
 
+# Question #3
 SELECT countries
 FROM library
 ORDER BY Jane Peacock;
 
+
+# Question #4
 SELECT customers
 FROM library
 WHERE company > 0;
 
+
+# Question #5
 SELECT AVG(money)
 FROM library
 GROUP BY customer
 HAVING gmail_adress
+
